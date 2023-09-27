bdb6184320bd2da0724e6f8aeb593cc9b1b99ebb
Robertino <gladdenrobertino@gmail.com>
2023-09-27 20:47:16 +0700

    initial commit

--- Tugas 02/02.py
+++ Tugas 02/02.py
@@ -11,4 +11,4 @@ nilai = int(input("Masukkan Nilai yang akan dicek :"))
 if nilai >= 10 and nilai <= 20 and nilai % 2 == 0:
     print(f"Bilangan {nilai} antara 10 sampai 20 dan bilangan genap")
 else:
-    print(f"{nilai} bukan bilangan antara 10 sampai 20 atau bilangan genap")
+    print(f"{nilai} bukan bilangan antara 10 sampai 20 atau bilangan ganjil")
